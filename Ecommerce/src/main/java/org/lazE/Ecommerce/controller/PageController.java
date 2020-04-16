b975ee483a6742dda1addd2d40d847e7e2f28d72
RohithKumar <rohithmadesh167@gmail.com>
2020-04-16 14:49:42 +0530

    Successfully Loaded Static Resources

---
+++ Ecommerce/src/main/java/org/lazE/Ecommerce/controller/PageController.java
@@ -0,0 +1,15 @@
+package org.lazE.Ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greetings","Welcome to SpringMVC");
+		return mv;
+	}
+}
---
+++ Ecommerce/src/main/java/org/lazE/Ecommerce/controller/package-info.java
@@ -0,0 +1,1 @@
+package org.lazE.Ecommerce.controller;
