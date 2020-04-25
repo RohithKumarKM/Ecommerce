d75a6e3fe3ab7a055b7334fc7d5a782bdcd1f432
RohithKumar <rohithmadesh167@gmail.com>
2020-04-26 00:26:32 +0530

    Final Commit

--- Ecommerce/src/main/java/org/lazE/Ecommerce/controller/CartController.java
+++ Ecommerce/src/main/java/org/lazE/Ecommerce/controller/CartController.java
@@ -15,84 +15,83 @@ import org.springframework.web.servlet.ModelAndView;
 public class CartController {
 
 	private final static Logger logger = LoggerFactory.getLogger(CartController.class);
-	
+
 	@Autowired
 	private CartService cartService;
+
 	@RequestMapping("/show")
 	public ModelAndView showCart(@RequestParam(name = "result", required = false) String result) {
-		
+
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Shopping Cart");
 		mv.addObject("userClickShowCart", true);
-		
-		if(result!=null) {
-			switch(result) {
-				case "added":
-					mv.addObject("message", "Product has been successfully added inside cart!");					
-					cartService.validateCartLine();
-					break;
-				case "unavailable":
-					mv.addObject("message", "Product quantity is not available!");					
-					break;
-				case "updated":
-					mv.addObject("message", "Cart has been updated successfully!");					
-					cartService.validateCartLine();
-					break;
-				case "modified":
-					mv.addObject("message", "One or more items inside cart has been modified!");
-					break;
-				case "maximum":
-					mv.addObject("message", "Maximum limit for the item has been reached!");
-					break;
-				case "deleted":
-					mv.addObject("message", "CartLine has been successfully removed!");
-					break;
+
+		if (result != null) {
+			switch (result) {
+			case "added":
+				mv.addObject("message", "Product has been successfully added inside cart!");
+				cartService.validateCartLine();
+				break;
+			case "unavailable":
+				mv.addObject("message", "Product quantity is not available!");
+				break;
+			case "updated":
+				mv.addObject("message", "Cart has been updated successfully!");
+				cartService.validateCartLine();
+				break;
+			case "modified":
+				mv.addObject("message", "One or more items inside cart has been modified!");
+				break;
+			case "maximum":
+				mv.addObject("message", "Maximum limit for the item has been reached!");
+				break;
+			case "deleted":
+				mv.addObject("message", "CartLine has been successfully removed!");
+				break;
 
 			}
-		}
-		else {
+		} else {
 			String response = cartService.validateCartLine();
-			if(response.equals("result=modified")) {
+			if (response.equals("result=modified")) {
 				mv.addObject("message", "One or more items inside cart has been modified!");
 			}
 		}
 
 		mv.addObject("cartLines", cartService.getCartLines());
 		return mv;
-		
+
 	}
-	
 
 	@RequestMapping("/{cartLineId}/update")
 	public String udpateCartLine(@PathVariable int cartLineId, @RequestParam int count) {
-		String response = cartService.manageCartLine(cartLineId, count);		
-		return "redirect:/cart/show?"+response;		
+		String response = cartService.manageCartLine(cartLineId, count);
+		return "redirect:/cart/show?" + response;
 	}
-	
+
 	@RequestMapping("/add/{productId}/product")
 	public String addCartLine(@PathVariable int productId) {
 		String response = cartService.addCartLine(productId);
-		return "redirect:/cart/show?"+response;
+		return "redirect:/cart/show?" + response;
 	}
-	
+
 	@RequestMapping("/{cartLineId}/remove")
 	public String removeCartLine(@PathVariable int cartLineId) {
 		String response = cartService.removeCartLine(cartLineId);
-		return "redirect:/cart/show?"+response;
+		return "redirect:/cart/show?" + response;
 	}
-	
-	/* after validating it redirect to checkout
-	 * if result received is success proceed to checkout 
-	 * else display the message to the user about the changes in cart page
-	 * */	
+
+	/*
+	 * after validating it redirect to checkout if result received is success
+	 * proceed to checkout else display the message to the user about the changes in
+	 * cart page
+	 */
 	@RequestMapping("/validate")
-	public String validateCart() {	
+	public String validateCart() {
 		String response = cartService.validateCartLine();
-		if(!response.equals("result=success")) {
-			return "redirect:/cart/show?"+response;
-		}
-		else {
+		if (!response.equals("result=success")) {
+			return "redirect:/cart/show?" + response;
+		} else {
 			return "redirect:/cart/checkout";
 		}
-	}	
+	}
 }
